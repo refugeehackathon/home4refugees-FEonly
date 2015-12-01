374856e819169ad47a93ea4a1bcb8c18ece8c2a5
hobbledehoy <hobbledehoy@mail.ru>
2015-12-01 15:17:29 +0100

    Revert "changes from whg"
    This reverts commit c44473360bf8b37e38130475245367baced33a66.

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
-<html lang="ar">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link href='http://fonts.googleapis.com/css?family=Lateef&subset=arabic,latin' rel='stylesheet' type='text/css'>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>course</title>
+    <title>Bootstrap 101 Template</title>
 
     <!-- Bootstrap -->
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/mystyles.css" rel="stylesheet">
-    <link href="css/mystyles.css" rel="stylesheet">
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -21,32 +17,12 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body id="home" class="main_image">
+  <body>
+    <h1>Hello, world!</h1>
 
-
-      <section class="container">
-         <div class="content row">
-
-          <section class="main col col-xs-12 " >
-          <?php include "_/components/php/header.php" ?>
-          <?php include "_/components/php/body.php" ?>
-          <!--
-          <?php include "_/components/php/footer.php" ?>
-        -->
-          </section> <!--  end of content -->
-
-
-
-         </div>  <!--  end of content -->
-
-
-      </section> <!--  Container -->
-
-      <!-- Footer -->
-      <?php include "_/components/php/footer.php" ?>
-
+  
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/bootstrap.js"></script>
       <script src="_/js/myscript.js"></script>
   </body>
 </html>
